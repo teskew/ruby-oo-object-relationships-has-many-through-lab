3e99cafdec178b38cae903ce2b6289e63f72423c
teskew <teskinder1@gmail.com>
2021-01-01 14:10:26 -0600

    commit tree 9be0a11b5dfb8acda6918f3413df2d07695128b4

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,29 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def songs
+      Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+      songs.map {|song| song.genre}
+  end
+  
+  def new_song(song, genre)
+      Song.new(name, self, genre)
+    end
+
+end
 
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,30 @@
 
+
+
+class Patient
+
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(name, doctor)
+    Appointment.new(name, self, doctor)
+  end
+end
+
